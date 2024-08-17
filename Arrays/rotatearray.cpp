d3df4fbe08e02684f17d1cc1180b25606f99e1f1
Renuka Deshpande <163507058+Renuka-deshpande@users.noreply.github.com>
2024-08-17 22:57:01 +0530

    Add files via upload

---
+++ Arrays/rotatearray.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotation(vector<int>& arr,int p){
+    int n=arr.size();
+    p=p%n;
+
+    reverse(arr.begin(),arr.begin()+p);
+    reverse(arr.begin()+p,arr.end());
+    reverse(arr.begin(),arr.end());
+
+}
+int main(){
+    vector<int> arr={1,2,3,4,5,6,7,8,9};
+    int p=4;
+    rotation(arr,p);
+
+    for (int i = 0; i <arr.size(); i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+//tc-o(n)
+//sc-o(1)
